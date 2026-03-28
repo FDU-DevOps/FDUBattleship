c0452963166d1356cee2f9184d54df16904e0b2d
Lizst-cv <windy33e@gmail.com>
2026-03-27 21:32:06 -0400

    DEVOPS-370 Add JUnit tests in AppTest to go over 70% threshold for JaCoCo

--- src/test/java/org/fdu/AppTest.java
+++ src/test/java/org/fdu/AppTest.java
@@ -3,11 +3,17 @@ package org.fdu;
 import org.junit.jupiter.api.Test;
 
  //import org.junit.jupiter.api.DisplayName;
- //import static org.junit.jupiter.api.Assertions.*;
+ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
     void testMain() {
         App.main(new String[]{});
+    }
+
+    @Test
+    void testGetStateNotNull() {
+        BattleBoard board = new BattleBoard();
+        assertNotNull(board.getState());
     }
 }
