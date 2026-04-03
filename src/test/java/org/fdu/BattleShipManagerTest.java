70d63cdf6ce19f54c34b16888d5e0c58ba2b1f55
Xavier-Orrala <xavierorrala12404@gmail.com>
2026-04-03 12:19:38 -0400

    DEVOPS-388 Updated test cases to fit new method formatting

--- src/test/java/org/fdu/BattleShipManagerTest.java
+++ src/test/java/org/fdu/BattleShipManagerTest.java
@@ -9,31 +9,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BattleShipManagerTest {
 
+    private PlayerDTO player;
     @Test
     @DisplayName("Validating user guesses.")
+
     void testValidatePlayerGuess()
     {
         BattleShipManager manager = new BattleShipManager();
+        BattleBoard board = new BattleBoard();
+        player = board.getState();
 
         // Out of Bounds Test Cases
-        // Example: A11 (OOB)
         String outOfBounds_3Index = "A11";
-        assertFalse(manager.validatePlayerGuess(outOfBounds_3Index));
+        player = manager.validatePlayerGuess(outOfBounds_3Index, player);
+        assertFalse(player.isValidGuess(), "Guess Out of Bounds should be False");
+
+        // Example: A100000 (out of bounds and beyond 3 index
+        String outOfBounds_Over3Index = "A100000";
+        player = manager.validatePlayerGuess(outOfBounds_Over3Index, player);
+        assertFalse(player.isValidGuess(), "Guess Out of Bounds should be False");
 
         // Example: A100 (oob)
         String outOfBounds_3Index_truncation = "A100";
-        assertFalse(manager.validatePlayerGuess(outOfBounds_3Index_truncation));
+        player = manager.validatePlayerGuess(outOfBounds_3Index_truncation, player);
+        assertFalse(player.isValidGuess(), "Guess Out of Bounds should be False");
 
-        // Wrong Format (Flipped col,row)
+        // Wrong Format (Flipped col,row) Test Cases
+        // Example: 10J, flipped format and 10 coordinate
         String flippedFormat_3Index = "10J";
-        assertFalse(manager.validatePlayerGuess(flippedFormat_3Index));
+        player = manager.validatePlayerGuess(flippedFormat_3Index, player);
+        assertFalse(player.isValidGuess(), "Guess should be in the correct col,row format");
 
+        // Example 5A, flipped format and 1-9 coordinate
         String flippedFormat = "5A";
-        assertFalse(manager.validatePlayerGuess(flippedFormat));
+        player = manager.validatePlayerGuess(flippedFormat, player);
+        assertFalse(player.isValidGuess(), "Guess should be in the correct col,row format");
 
-        
 
-        // Example: A1000000 (OOB AND more than index 3)
     }
 
 
